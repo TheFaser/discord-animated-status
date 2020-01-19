5ef60a55c4dc4a9f2ceedd62ced39fd325ec8e72
frenzy <52493771+eternalfrenzy@users.noreply.github.com>
2020-01-19 13:09:51 +0300

    Delete frameparser.py

--- frameparser.py
+++
@@ -1,7 +0,0 @@
-from required import *
-
-def parse_frame(frame):
-    now = datetime.datetime.now()
-    frame["str"] = frame["str"].replace("#curtime#", datetime.datetime.strftime(now, "%H:%M"))
-    frame["str"] = frame["str"].replace("#servcount#", str(len(requests.get(api_url+"/users/@me/guilds", headers=auth("get")).json(encoding="utf-8"))))
-    return frame
